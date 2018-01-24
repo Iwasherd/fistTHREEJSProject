a5243631c1c97de4665cdf174af15ae8090394e1
Ivan <31006937+Iwasherd@users.noreply.github.com>
2018-01-24 20:49:30 +0300

    fixed bags

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 (function() {
               const height = document.querySelector("#world").offsetHeight,
                 width = document.querySelector("#world").offsetWidth,
-                sniper = document.querySelector(".sniper");
-                sniper180 = sniper.classList.add('sniper180');
-
+                sniper = document.querySelector(".sniper"),
+                 sniper180 = sniper.classList.add('sniper180');
+              
               let scene = new THREE.Scene();
               let camera = new THREE.OrthographicCamera(width / -7, width / 7, height / 7, height / -7, 1, 1000);
 
@@ -23,17 +23,13 @@
               scene.add(plane);
 
               ///road///
-              let roadVertical = new THREE.Mesh(new THREE.CubeGeometry(115, 40, 20, 1, 1, 1, new THREE.MeshBasicMaterial(
-                    { color: 0x00ff33 }
-                  )));
+              let roadVertical = new THREE.Mesh(new THREE.CubeGeometry(115, 40, 20, 1, 1, 1));
                   roadVertical.position.x = 40;
                   roadVertical.position.y = 85;
                   roadVertical.position.z = 103;
                   scene.add(roadVertical);
 
-              let roadGorizontal = new THREE.Mesh(new THREE.CubeGeometry(10, 20, 215, 1, 1, 1, new THREE.MeshBasicMaterial(
-                    { color: 0x00ff33 }
-                  )));
+              let roadGorizontal = new THREE.Mesh(new THREE.CubeGeometry(10, 20, 215, 1, 1, 1));
                     roadGorizontal.position.x = 40;
                     roadGorizontal.position.y = 22.5;
                     roadGorizontal.position.z = 0;
@@ -52,49 +48,49 @@
 
             function PlayerMoving() {
                 let topCoordinates = sniper.getBoundingClientRect().top,
-                  rightCoordinates = sniper.getBoundingClientRect().right,
-                  textDown = document.querySelector(".text-down"),
+                  rightCoordinates = sniper.getBoundingClientRect().right;
+                   textDown = document.querySelector(".text-down"),
                   textRight = document.querySelector(".text-right"),
                   textOver = document.querySelector(".text-over"),
-                  right = 134;
                   console.log(rightCoordinates);
+                  console.log(topCoordinates);
+                  sniperTop = 54;
+                  sniperLeft = 130;
+                  
 
                          keyboardJS.bind("down", (e)=> {
                              console.log("down is pressed");
-                             if (topCoordinates >= height / 2) {
+                             if ((sniper.style.top = sniperTop + "px" == 390 + "px")) {
                                sniper.style.animation = "none";
                                sniper.classList.add("sniper270");
-                                textDown.style.display = 'none';
-                                textRight.style.display = 'block';
-                               
-                                    keyboardJS.bind("right", (e) => {
-                                        console.log("right is pressed");
-                                        if (rightCoordinates >= width / 1.5) {
-                                          sniper.classList.add("sniperDeath");
-                                          sniper.style.animation = "sniperDeath .8s steps(25)";
-                                          sniper.style.top = 325 + 'px';
-                                          textRight.style.display = "none";
-                                          textOver.style.display = "block";
-                                          keyboardJS.unbind("right", previouslyBoundHandler);
-                                        } else {
+                               textDown.style.display = "none";
+                               textRight.style.display = "block";
 
-                                           rightCoordinates += 0.15;
-                                          sniper.style.animation = "sniper270 0.8s steps(25) infinite";
-                                          sniper.style.left = rightCoordinates + "px";
-                                          console.log(rightCoordinates);
-                                        }
-                                      }, (e) => {
-                                           
-                                              sniper.style.animation = "none";
-                                          
-                                        console.log("a is released");
-                                      });
-                               
+                               keyboardJS.bind("right", e => {
+                                   console.log("right is pressed");
+                                   if ((sniper.style.left = sniperLeft + "px" >= 760 + "px")) {
+                                     sniper.classList.add("sniperDeath");
+                                     sniper.style.animation = "sniperDeath .8s steps(25)";
+                                     sniper.style.top = 325 + "px";
+                                     textRight.style.display = "none";
+                                     textOver.style.display = "block";
+                                     keyboardJS.unbind("right", previouslyBoundHandler);
+                                   } else {
+                                     sniperLeft += 0.2;
+                                     sniper.style.animation = "sniper270 0.8s steps(25) infinite";
+                                     sniper.style.left = sniperLeft + "px";
+                                     console.log(rightCoordinates);
+                                   }
+                                 }, e => {
+                                   sniper.style.animation = "none";
+
+                                   console.log("a is released");
+                                 });
                              } else {
-                                 
-                               topCoordinates += 1.5;
+                               sniperTop += 1.5;
                                sniper.style.animation = "sniper180 0.8s steps(25) infinite";
-                               sniper.style.top = topCoordinates + "px";
+                               sniper.style.top = sniperTop + "px";
+                               console.log(topCoordinates);
                              }
                            }, (e)=> {
                                if (sniper.classList.contains('sniperDeath')) {
